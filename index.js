460a9fc7639eca431ee5408bd0cd354e256d46f0
GuilhermeZampar <145402235+Gui-Z07@users.noreply.github.com>
2024-03-19 21:27:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function changeImg(img) {
+  document.querySelector(".tea-logo img").src = `./img/img${img}.png`;
+
+  if (img == 1) {
+    document.querySelector(".waning-moon-017143").style.background = "#017143";
+  }
+  if (img == 2) {
+    document.querySelector(".waning-moon-017143").style.background = "pink";
+  }
+  if (img == 3) {
+    document.querySelector(".waning-moon-017143").style.background = "#d752b1";
+  }
+}
