416a1d536bdb8886e338426033bd219eb943975d
GuilhermeZampar <145402235+Gui-Z07@users.noreply.github.com>
2024-03-21 19:03:43 -0300

    add mq

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function changeImg(img) {
   document.querySelector(".tea-logo img").src = `./img/img${img}.png`;
 
   if (img == 1) {
-    document.querySelector(".waning-moon-017143").style.background = "#017143";
+    document.querySelector(".waning-moon-017143").style.background = "#017142be";
   }
   if (img == 2) {
     document.querySelector(".waning-moon-017143").style.background = "pink";
